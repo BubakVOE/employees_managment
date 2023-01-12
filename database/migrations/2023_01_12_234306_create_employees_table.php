81953c2fb22bf733882a594bfec3d53a17cce77d
BubakVOE <ludwigtom@seznam.cz>
2023-01-13 00:52:56 +0100

    added migrations

---
+++ database/migrations/2023_01_12_234306_create_employees_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->char('zip_code');
+            $table->date('birt_date');
+            $table->date('date_hired');
+
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
